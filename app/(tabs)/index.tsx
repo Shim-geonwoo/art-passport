6b1a59bdcd7480b9700e88de085e72dcc5b73724
shimgeonwoo <geonwoo1030@gmail.com>
2026-07-13 13:11:09 +0900

    첫 커밋: 프로젝트 초기 세팅

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome to Art port!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
